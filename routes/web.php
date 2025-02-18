1e66908720952c1cf74801d619bcfe10e4a321b9
VozSoldat <muhamaderril7@gmail.com>
2025-02-18 10:11:28 +0700

    tugas (optional parameters)

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,9 @@ Route::get('/about', function(){
     return "NIM: 2341720183\nNama: Muhammad Erril Putra Pratidina";
 });
 
-Route::get('/user/{name}', function($name){
-    return "Nama saya ". $name;
-});
+// Route::get('/user/{name}', function($name){
+//     return "Nama saya ". $name;
+// });
 
 Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
     return 'Pos ke-'.$postId.' Komentar ke-' . $commentId;
@@ -43,4 +43,8 @@ Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
 
 Route::get('/articles/{id}', function($id){
 return "Halaman artikel dengan ID ". $id;
+});
+
+Route::get('/user/{name?}', function($name = "John"){
+    return "Nama saya ". $name;
 });
